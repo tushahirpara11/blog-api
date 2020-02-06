196bff021fc5d48a0edaf6fcc26106730cb0a526
tusharhirpara11 <tusharhirpara98@gmail.com>
2020-02-06 15:18:29 +0530

    remove duplicate user

--- controller/usersController.js
+++ controller/usersController.js
@@ -5,26 +5,32 @@ const userModel = require('../model/userModel');
 const { Message } = require('../commonFunction/commonFunction');
 
 async function addUser(req, res) {
-  console.log(req.body);
-  let addUser = new userModel(req.body);
-  try {
-    const userData = await addUser.save();
-    res.json(Message(200, "true", "User Added", userData));
-  } catch (err) {
-    res.json(Message(false, "Error", err));
+  const count = await userModel.find({ userName: req.body.userName, contact: req.body.contact })
+  if (count.length != 1) {
+    let addUser = new userModel(req.body);
+    try {
+      const userData = await addUser.save();
+      res.json(Message(200, "true", "User Added", userData));
+    } catch (err) {
+      res.json(Message(false, "Error", err));
+    }
+  } else {
+    res.json(Message(false, "Data already Exists"));
   }
 }
 
 async function authenticate(req, res) {
   try {
     const user = await userModel.findOne({ userName: req.body.userName, password: req.body.password }, { password: 0 });
-    if (user != null) {      
-      jwt.sign({user}, process.env.SECRET_KEY, function (err, token) {
-        res.json(Message(200, 'OK', 'Token Created', token));
+    if (user != null) {
+      jwt.sign({ user }, process.env.SECRET_KEY, function (err, token) {
+        res.json(Message(200, true, 'OK', 'Token Created', token));
       });
+    } else {
+      res.json(Message(400, false, 'OK', 'User Not Found'));
     }
   } catch (err) {
-    res.json(message(400, 'Bad Request', 'User not exists'));
+    res.json(message(400, false, 'Bad Request', 'User not exists'));
   }
 }
 
